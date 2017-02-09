c247b4ad96d26b9386e824d2d3d5a929370bd15a
JackTam <371997352@qq.com>
2017-02-09 17:33:22 +0800

    add flex grid and fulltable

---
+++ src/fullTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <atable></atable>
+  <apagination></apagination>
+  </div>
+</template>
+
+<script>
+  import atable from 'components/table.vue'
+  import apagination from 'components/pagination.vue'
+
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    components:{
+      atable,
+      apagination
+    }
+  }
+</script>
