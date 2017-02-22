5f5e2b51592c047016f134672e2c9108dea2ad0e
Jack <371997352@qq.com>
2017-02-22 22:35:45 +0800

    add socket

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,22 +6,40 @@
     <mu-float-button icon="add" secondary mini class="demo-float-button"/>
     <mu-float-button icon="star" disabled class="demo-float-button"/>
     <mu-float-button icon="star" disabled mini class="demo-float-button"/>
+    <div class="bottom">
+    <mu-text-field hintText="提示文字" v-model="send"/>
+      <mu-raised-button label="发送" @click="sendmsg()"/>
+    </div>
   </div>
 </template>
 
 <script>
-
+  var socket = io.connect('http://localhost:8888');
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      send:''
     }
+  },
+  methods:{
+    sendmsg() {
+      var self = this;
+      console.log(this.send);
+      socket.emit('myother', { my: self.send });
+    }
+  },
+  created(){
+
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .bottom{
+    bottom: 3% !important;
+  }
 </style>
---
+++ src/socket.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jack on 2017/2/22.
+ */
+var socket = io.connect('http://localhost:8888');
+
+export default socket;
